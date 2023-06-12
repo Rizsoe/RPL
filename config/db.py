bf4d0e46829f6d5d0611ec78eaa97e0934ee3669
Muhamad Rizal Soeid <Rizalsoeid@gmail.com>
2023-06-13 07:57:05 +0800

    init

--- config/db.py
+++ config/db.py
@@ -3,11 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import pymysql.cursors
 
-host='localhost'
+host='containers-us-west-110.railway.app'
 user='root'
-port= 3306
-password=''
-db='absensi_db'
+port= 6395
+password='5awcXt09ZHlr1xwxZLie'
+db='railway'
 # SQLALCHEMY_DATABASE_URL = ("mysql+pymysql://<username>:<password>@<host>/<dbname>[?<options>]")
 SQLALCHEMY_DATABASE_URL = (f"mysql+pymysql://{user}:{password}@{host}:{port}/{db}")
 # # membuat koneksi ke MySQL
